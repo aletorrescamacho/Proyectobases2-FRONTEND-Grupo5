51223313bc72ef3d263a529df164465bfa15d0ef
Luis Soriano <l.soriano@correo.unimet.edu.ve>
2024-11-13 17:36:12 -0400

    inicio de sesion totalmente funcional

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Form } from '@/components/Form'
+import { Form, FormValues } from '@/components/Form'
 import { useAuthFetch } from '@/hooks/useAuthFetch'
 import { useLoading } from '@/hooks/useLoading'
 
@@ -9,20 +9,35 @@ export default function LoginPage () {
   const authFetch = useAuthFetch()
 
   const login = async (formData: any) => {
-    startLoading()
-    await authFetch({
-      endpoint: 'login',
-      redirectRoute: '/home',
-      formData
-    })
-    finishLoading()
-  }
+    startLoading();
+
+    try {
+      await authFetch({
+        endpoint: 'users/login', // Cambia esto al endpoint correcto de tu backend
+        formData,
+        redirectRoute: '/home', // Ruta a la que quieres redirigir después de un registro exitoso
+      });
+    } catch (error) {
+      console.error('Error en el Inicio de Sesion', error);
+    } finally {
+      finishLoading();
+    }
+  };
+
+  const handleSubmit = (values: FormValues) => {
+    const formData = {
+      email: values.email,
+      password: values.password,
+    };
+  
+    login(formData);
+  };
 
   return (
     <>
       <Form
         title='Inicia Sesión'
-        onSubmit={login}
+        onSubmit={handleSubmit}
         description='Formulario para iniciar sesión'
       >
         <div className='my-[10px] flex flex-col gap-4'>
@@ -40,7 +55,7 @@ export default function LoginPage () {
         </div>
         <Form.SubmitButton buttonText='Iniciar Sesión' isLoading={isLoading} />
         <Form.Footer
-          description='Te olvidate tu contraseña?'
+          description='Olvidaste tu contraseña?'
           link='/forget-password'
           textLink='Recuperar contraseña'
         />
